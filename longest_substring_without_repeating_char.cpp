9dd04886daefbca2e8b4745360102813f11b09d9
sylcrq <syl272365943@126.com>
2015-02-25 17:29:31 +0800

    *): Update "Longest Substring Without Repeating Characters"

--- longest_substring_without_repeating_char.cpp
+++ longest_substring_without_repeating_char.cpp
@@ -1,44 +1,78 @@
 #include <string>
 #include <string.h>
-#include <iostream>
-
+//#include <iostream>
 
 using namespace std;
 
+// 时间复杂度: O(N)
+// 一种更好的解法: http://blog.csdn.net/linhuanmars/article/details/19949159
+// Leetcode Run Time: 20ms, 2015-02-25
+int lengthOfLongestSubstring_ii(string s)
+{
+    int size = s.size();
+    int max = 0;
+    char map_char[256] = {0};  // hash map
+
+    int left = 0;
+    int right = 0;
+
+    while(right < size)
+    {
+        int index = s[right];
+
+        if(map_char[index] == 0)
+        {
+            map_char[index] = 1;
+            right++;
+        }
+        else
+        {
+            max = (right-left > max) ? (right-left) : max;
+
+            while(map_char[index] != 0)
+            {
+                int tmp = s[left];
+
+                map_char[tmp] = 0;
+                left++;
+            }
+        }
+    }
+
+    max = (right-left > max) ? (right-left) : max;
+
+    return max;
+}
+
+// 最差时间复杂度: O(N^2)
+// 代码调整后Leetcode Run Time:50ms+, 2015-02-25
 int lengthOfLongestSubstring(string s)
 {
-    int len = 0;
     int max = 0;
-
-    char map_char[256] = {0};
+    char map_char[256] = {0};  // hash map
 
     for(size_t i=0; i<s.length(); i++)
     {
-        len = 0;
+        int len = 0;
         memset(map_char, 0, 256);
 
         for(size_t j=i; j<s.length(); j++)
         {
-            if(map_char[s[j]] == 0)
-            {
-                map_char[s[j]] = 1;
-                len++;
+            int index = s[j];
 
-                //最后一个字符，更新max
-                if(j == (s.length() - 1))
-                    max = (len > max) ? len : max;
+            if(map_char[index] == 0)
+            {
+                map_char[index] = 1;
+                len++;
             }
             else
             {
-                max = (len > max) ? len : max;
                 break;
             }
         }
 
-        //cout << max << ",";
+        max = (len > max) ? len : max;
     }
-
-    //cout << endl;
 
     return max;
 }
--- unit_test/longest_substring_without_repeating_char_test.cpp
+++ unit_test/longest_substring_without_repeating_char_test.cpp
@@ -5,19 +5,25 @@
 using namespace std;
 
 int lengthOfLongestSubstring(string s);
+int lengthOfLongestSubstring_ii(string s);
 
 TEST(LongestSubstringWithoutRepeatingCharTestCase, Normal)
 {
     EXPECT_EQ(3, lengthOfLongestSubstring("abcabcbb"));
     EXPECT_EQ(1, lengthOfLongestSubstring("bbbbb"));
-
     EXPECT_EQ(12, lengthOfLongestSubstring("wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco"));
-
     EXPECT_EQ(12, lengthOfLongestSubstring("qopubjguxhxdipfzwswybgfylqvjzhar"));
+
+    EXPECT_EQ(3, lengthOfLongestSubstring_ii("abcabcbb"));
+    EXPECT_EQ(1, lengthOfLongestSubstring_ii("bbbbb"));
+    EXPECT_EQ(12, lengthOfLongestSubstring_ii("wlrbbmqbhcdarzowkkyhiddqscdxrjmowfrxsjybldbefsarcbynecdyggxxpklorellnmpapqfwkhopkmco"));
+    EXPECT_EQ(12, lengthOfLongestSubstring_ii("qopubjguxhxdipfzwswybgfylqvjzhar"));
 }
 
 TEST(LongestSubstringWithoutRepeatingCharTestCase, EmptyString)
 {
     string str;
     EXPECT_EQ(0, lengthOfLongestSubstring(str));
+
+    EXPECT_EQ(0, lengthOfLongestSubstring_ii(str));
 }
